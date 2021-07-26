d4114928794e51f6da4f333f4938a5d3f24cd1da
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-07-26 10:10:34 +0700

    Update weleh.sh

--- weleh.sh
+++ weleh.sh
@@ -4,20 +4,20 @@
 cd
 
 #Install Script Websocket-SSH Python
-wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/openssh-socket.py
+#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/openssh-socket.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/ws-stunnel
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
-chmod +x /usr/local/bin/ws-openssh
+#chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 #chmod +x /usr/local/bin/ws-ovpn
 
 
 #System OpenSSH Websocket-SSH Python
-wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
+#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
@@ -32,9 +32,9 @@ wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com
 #
 systemctl daemon-reload
 #Enable & Start & Restart ws-openssh service
-systemctl enable ws-openssh.service
-systemctl start ws-openssh.service
-systemctl restart ws-openssh.service
+#systemctl enable ws-openssh.service
+#systemctl start ws-openssh.service
+#systemctl restart ws-openssh.service
 
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
