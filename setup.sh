2017cda388fa90700a871f3bfa5492e067f9bfec
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-09-21 19:44:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,21 +30,21 @@ echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/weleh.sh && chmod +x weleh.sh && screen -S weleh ./weleh.sh
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ohp.sh && chmod +x ohp.sh && screen -S ohp ./ohp.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/weleh.sh && chmod +x weleh.sh && screen -S weleh ./weleh.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ohp.sh && chmod +x ohp.sh && screen -S ohp ./ohp.sh
 #install ssr
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 #install trojan-go
-#wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/ins-trojango.sh && chmod +x ins-trojango.sh && screen -S Trojan-go ./ins-trojango.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/ins-trojango.sh && chmod +x ins-trojango.sh && screen -S Trojan-go ./ins-trojango.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
